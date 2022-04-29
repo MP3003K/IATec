3aa0b7ec00e22945a97c86e40faea986b6297be8
Korth <mijailpalomino@upeu.edu.pe>
2022-04-28 21:26:13 -0500

    Ejemplo de Routing I.2

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+
+  }
+  volverHome(){
+    this.router.navigate([''])
+  }
 
   ngOnInit(): void {
   }
