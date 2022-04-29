c6e1289e90ae75bc97399ddf6aee0d263572aac8
Korth <mijailpalomino@upeu.edu.pe>
2022-04-28 21:02:10 -0500

    Ejemplo de Routing I

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes=[
+  { path:'', component: HomeComponent},
+  { path:'sidebar', component:SidebarComponent},
+  { path:'header', component:HeaderComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
